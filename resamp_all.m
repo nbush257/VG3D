de4c501062717696d6681a3aa4a1def92719bf5d
nbush257 <nbush257@gmail.com>
2017-09-13 17:21:14 -0500

    resamp_all now calls filtervars instead of performing the filtering on its own. This should make things cleaner.

--- resamp_all.m
+++ resamp_all.m
@@ -1,10 +1,13 @@
 function resamp_all(fname_in)
 % This function saves the important data for E3D and neural, and samples
-% everything at 1K
-smooth_span = 11;
+% everything at the desired rate (suggested 1K, but flexible if needed:
+sgolay_span = 11;
+hampel_k = 3;
 new_sr = 1000; %in Hz
 nan_gap = 20; % in frames [333 ms for 300fps, 200ms for 500 fps
 outname = [fname_in(1:end-12) '1K.mat'];
+%% DO NOT EDIT BELOW THIS LINE IF YOU CHANGE PARAMETERS
+% ===================================================
 %%
 vars.M = M;
 vars.F = F;
@@ -20,17 +23,11 @@ filtvars = vars;
 
 
 for ii = 1:length(varnames)
-    for jj =1:size(filtvars.(varnames{ii}),2)
-        filtvars.(varnames{ii})(:,jj) = InterpolateOverNans(filtvars.(varnames{ii})(:,jj),nan_gap);
-    end
-    idx_mask = isnan(filtvars.(varnames{ii}));
-    
-    filtvars.(varnames{ii}) = hampel(filtvars.(varnames{ii}));
-    
-    for jj = 1:size(filtvars.(varnames{ii}),2)
-        filtvars.(varnames{ii})(:,jj) = smooth(filtvars.(varnames{ii})(:,jj),smooth_span,'sgolay');
-    end
-    filtvars.(varnames{ii})(idx_mask) = NaN;
+    filtvars.(varnames{ii}) = filtervars(vars.(varnames{ii}),...
+        'nan_gap',nan_gap,...
+        'sgolay_span',sgolay_span,...
+        'hampel_k',hampel_k...
+    );
 end
 
 %% resample vars
@@ -39,13 +36,15 @@ for ii = 1:length(varnames)
     vars.(varnames{ii}) = resamp(vars.(varnames{ii}),spt{1},sr,frametimes,new_sr);
     filtvars.(varnames{ii}) = resamp(filtvars.(varnames{ii}),spt{1},sr,frametimes,new_sr);
 end
+
+% resample the spike times
 for ii = 1:length(spt)
     [~,~,spt_upsamp{ii}] = resamp(rawvars.M(:,1),spt{ii},sr,frametimes,new_sr);
 end
-
+% resample the contact var
 C_raw=C;
 C = resamp(C,spt{1},sr,frametimes,new_sr);
-%%
+%% save the output
 sp = spt_upsamp;
 save(outname,'vars','rawvars','filtvars','sp','sr','C','C_raw','PT');
 
