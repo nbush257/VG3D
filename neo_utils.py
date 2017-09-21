940f32794963242c2b917bb668b5b7e9e9a851ac
nbush257 <nbush257@gmail.com>
2017-09-21 16:55:06 -0500

    created a utilities module that will help me quickly play with the data. The getVar function will extract an analog variable and join it or not if you choose.

---
+++ neo_utils.py
@@ -0,0 +1,26 @@
+from neo.io import NeoMatlabIO as NIO
+from neo.core import Block
+
+def getVar(blk,varname='M',join=True,keep_neo=False):
+    ''' use this utility to access an analog variable from all segments in a block easily
+    If you choose to join the segments, returns a list of '''
+    varnames = ['M','F','PHIE','TH','Rcp','THcp','PHIcp']
+    idx = varnames.index(varname)
+    split_points = []
+    var = []
+    if keep_neo and join:
+        raise ValueError('Cannot join an keep neo structure')
+
+    for seg in blk.segments:
+        if keep_neo:
+            var.append(seg.analogsignals[idx])
+        else:
+            var.append(seg.analogsignals[idx].as_array())
+            split_points.append(seg.analogsignals[idx].shape[0])
+    if join:
+        var = np.concatenate(var,axis=0)
+        return (var, split_points)
+    else:
+        return var
+
+
