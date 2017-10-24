f63d41901c82245d14c7eeed91ce38d951426fdb
nbush257 <nbush257@gmail.com>
2017-10-24 18:06:11 -0500

    changed batch_run_script so that the call to the path adds a forward slash

--- scripts/batch_run_models.sh
+++ scripts/batch_run_models.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-export DATA_PATH="$BOX_PATH""__VG3D/deflection_trials/data"
+export DATA_PATH="$BOX_PATH""/__VG3D/deflection_trials/data"
 for filename in "$DATA_PATH"/rat*2017_08*D1*.pkl; do 
-	# echo $(basename "$filename")
+	echo $(basename "$filename")
 	python modelling.py "$filename" --STM -C -P -w 5 --varlist M,F -p 5ms_all
 	python modelling.py "$filename" --STM -C -P -w 5 --varlist M,F -D -p 5ms_all_deriv
 done
