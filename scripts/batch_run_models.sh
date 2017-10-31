305eb6609c2489e92b4f46c690a18551b507af84
nbush257 <nbush257@gmail.com>
2017-10-31 10:56:56 -0500

    added a function to incorporate spike history into the design matrix. added a way to implement a delay when applying the pillow bases. refactored a matlib call and import

--- scripts/batch_run_models.sh
+++ scripts/batch_run_models.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 export DATA_PATH="$BOX_PATH""/__VG3D/deflection_trials/data"
-for filename in "$DATA_PATH"/rat*.pkl; do 
+for filename in "$DATA_PATH"/rat*2017_08*D1*.pkl; do 
 	# echo $(basename "$filename")
-	python modelling.py "$filename" --STM -C -P -w 5 --varlist M,F --plot_tgl -p 5ms_all
-	python modelling.py "$filename" --STM -C -P -w 5 --varlist M,F -D --plot_tgl -p 5ms_all_deriv
+	# python modelling.py "$filename" --STM -C -P --GLM -w 5 --varlist M,F --plot_tgl -p 5ms_all
+	# python modelling.py "$filename" --STM -C -P --GLM -w 5 --varlist M,F -D --plot_tgl -p 5ms_all_deriv
+	python modelling.py "$filename" --STM -w 1 --varlist M,F -D --plot_tgl -p 1ms_stm_deriv --silence_noncontact
 done
