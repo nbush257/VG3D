22ee2ad4e9675abc1f4b02b0fbbd27b78552b60b
nbush257 <nbush257@gmail.com>
2017-11-15 17:52:24 -0500

    added a comments to several modules in neo_utils

--- modules/neo_utils.py
+++ modules/neo_utils.py
@@ -16,18 +16,33 @@ proc_path =os.environ['PROC_PATH']
 sys.path.append(os.path.join(proc_path,r'VG3D\modules'))
 sys.path.append(os.path.join(proc_path,r'VG3D\scripts'))
 
-def get_blk(f):
+def get_blk(f='rat2017_08_FEB15_VG_D1_NEO.pkl',fullname=False):
+    '''loads in a NEO block from a pickle file. Calling without arguments pulls in a default file'''
     box_path = os.environ['BOX_PATH']
     dat_path = os.path.join(box_path,r'__VG3D\deflection_trials\data')
-    fid = PIO(os.path.join(dat_path,f))
+    if not fullname:
+        fid = PIO(os.path.join(dat_path,f))
+    else:
+        fid = PIO(f)
+
     return fid.read_block()
 
 def get_rate_b(blk,unit_num,sigma=10*pq.ms):
+    '''
+    convinience function to get a standard rate and binary spike vector from a block and unit number
+
+    :param blk: neo block
+    :param unit_num: int dictating which unit to load
+    :param sigma: gaussian kernal smoothing parameter. Must be a quantity
+    :return:    r - soothed spike rate
+                b - binary spike vector
+    '''
     sp = concatenate_sp(blk)['cell_{}'.format(unit_num)]
     kernel = elephant.kernels.GaussianKernel(sigma=sigma)
     b = elephant.conversion.binarize(sp,sampling_rate=pq.kHz)[:-1]
     r = elephant.statistics.instantaneous_rate(sp,sampling_period=pq.ms,kernel=kernel).magnitude.squeeze()
     return(r,b)
+
 def get_var(blk,varname='M',join=True,keep_neo=True):
     ''' use this utility to access an analog variable from all segments in a block easily
     If you choose to join the segments, returns a list of '''
@@ -79,6 +94,11 @@ def concatenate_sp(blk):
     return sp
 
 def concatenate_epochs(blk):
+    '''
+    takes a block which may have multiple segments and returns a single Epoch which has the contact epochs concatenated and properly time aligned
+    :param blk: a neo block
+    :return: contact -- a single neo epoch array
+    '''
     starts = np.empty([0])
     durations = np.empty([0])
     t_start = 0*pq.ms
@@ -108,18 +128,39 @@ def nan_helper(y):
 
     return np.isnan(y), lambda z: z.nonzero()[0]
 def nan_bounds(var):
+    '''
+    Grabs the boundaries of the NaNs (where nan sections start and stop
+    :param var: Vector of a signal where we want to know where NaN sections start and stop
+    :return: starts, stops -- indices of the input array where nan sections start and stop
+    '''
     if var.ndim>1:
         raise ValueError('input needs to be 1D')
     nans = nan_helper(var)[0].astype('int')
     d = np.diff(np.concatenate([[0],nans]))
     return np.where(d==1)[0],np.where(d==-1)[0]
 
-def replace_NaNs(var, mode='zero'):
+def replace_NaNs(var, mode='zero',pad=20):
+    '''
+    takes a Vector, array, or neo Analog signal and replaces the NaNs with desired values
+    :param var: numpy array or neo analog signal to replace NaNs
+    :param mode: string indicating what to replace NaNs with:
+                    'zero'      --  replace all NaNs with zero
+                    'median'    --  replace all NaNs with vector's median. Assumes matrix columns are different variables
+                    'rm'        --  removes all NaNs CHANGES THE SHAPE OF THE ARRAY
+                    'interp'    --  linear interpolation over NaN sections using end points
+                    'pchip'     --  spline interpolation over NaN secitions using endpoints. Uses PAD number of samples on either side of the gap to compute the spline
+    :param pad: used in pchip interpolation to determine how much data to use when creating spline
+    :return:    data -- the interpolated input data, in the same type as was input.
+
+    '''
+
+    # copy input to new variable to prevent inplace operations and convert from NEO if needed.
     if type(var)==neo.core.analogsignal.AnalogSignal:
-        data = var.as_array().copy()
+        data = var.magnitude.copy()
     else:
         data = var.copy()
 
+    # Apply replacement depending on mode
     if mode=='zero':
         data[np.isnan(data)]=0
     elif mode=='median':
@@ -134,7 +175,6 @@ def replace_NaNs(var, mode='zero'):
             nans, x = nan_helper(data[:, ii])
             data[nans, ii] = np.interp(x(nans), x(~nans), data[~nans, ii])
     elif mode=='pchip':
-        pad=20
         for ii in xrange(data.shape[1]):
             starts,stops = nan_bounds(data[:, ii])
             for start,stop in zip(starts,stops):
@@ -143,9 +183,11 @@ def replace_NaNs(var, mode='zero'):
 
                 x = np.arange(start,stop)
                 y = scipy.interpolate.pchip_interpolate(xi,yi,x)
+    # catch undefined modes
     else:
         raise ValueError('Wrong mode indicated. May want to impute NaNs in some instances')
 
+    # map output to neo signal if needed
     if type(var)==neo.core.analogsignal.AnalogSignal:
         var_out = neo.core.AnalogSignal(data*var.units,
                                         t_start=0.*pq.ms,
@@ -157,22 +199,44 @@ def replace_NaNs(var, mode='zero'):
 
 
 def create_unit_chan(blk):
+    '''
+    Creates and appends a channel index given a block. This should only be run on brand new data.
+    :param blk:
+    :return:
+    '''
+    if True:
+        raise Exception('I think this is depricated. Be sure that the creation of blocks from the matlab files is correct')
+
+    # init
     chx = ChannelIndex(0,name='Units')
     num_units = []
+
+    # grab all the spike trians
     for seg in blk.segments:
         num_units.append(len(seg.spiketrains))
+
+
     num_units = max(num_units)
+
+    # append units to channel index
     for ii in xrange(num_units):
         unit = Unit(name='cell_{}'.format(ii))
         chx.units.append(unit)
+
+    # append all spike trains to appropriate units in the channel index
     for seg in blk.segments:
         for ii,train in enumerate(seg.spiketrains):
             chx.units[ii].spiketrains.append(train)
+
+    # append the unit channel (contains all the units) to the block
     blk.channel_indexes.append(chx)
 
 
 def create_analog_chan(blk):
     '''maps the mechanical and kinematic signals to a channel index.'''
+    if True:
+        raise Exception('I think thi is depricated. It does not account for all the variables we want to save.')
+
     varnames = ['M','F','PHIE','TH','Rcp','THcp','PHIcp']
     for ii in range(len(varnames)):
         chx = ChannelIndex(0,name=varnames[ii])
@@ -188,20 +252,34 @@ def add_channel_indexes(blk):
 
 
 def get_Cbool(blk):
+    '''
+    Given a block,get a boolean vector of contact for the concatenated data
+    :param blk: neo block
+    :return: Cbool - a boolean numpy vector of contact
+    '''
     Cbool = np.array([],dtype='bool')
-
+    # Get contact from all available segments and offset appropriately
     for seg in blk.segments:
         seg_bool = np.zeros(len(seg.analogsignals[0]),dtype='bool')
         epochs = seg.epochs[0]
+
+        # Set the samples during contact to True
         for start,dur in zip(epochs,epochs.durations):
             start = int(start)
             dur = int(dur)
             seg_bool[start:start+dur]=1
+
         Cbool = np.concatenate([Cbool,seg_bool])
     return Cbool
 
 
 def get_root(blk,cell_no):
+    '''
+    Utility function that gets a unique ID string for each cell
+    :param blk: neo block of the data
+    :param cell_no: index of the cell number
+    :return: root - a unique string ID
+    '''
     return(blk.annotations['ratnum'] + blk.annotations['whisker'] + 'c{:01d}'.format(cell_no))
 
 def import_data_to_model(file,vars=['M','F'],unit_idx=0):
