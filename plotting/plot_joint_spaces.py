fb344380d9f548ee2a66c3677097dc86b5ba19b2
nbush <nbush257@gmail.com>
2018-02-09 07:40:59 -0600

    Lots of changes to plot the deritave and joint spaces

---
+++ plotting/plot_joint_spaces.py
@@ -0,0 +1,137 @@
+from analyze_by_deflection import *
+import varTuning
+import quantities as pq
+import matplotlib.ticker as mtick
+# ============================ #
+# edit here #
+# ============================ #
+save_loc = os.path.join(os.environ['BOX_PATH'],'__VG3D/_deflection_trials/_NEO/results')
+plt.rcParams['svg.fonttype'] = 'none'
+plt.rcParams['axes.titlesize'] = 12
+plt.rcParams['figure.titlesize'] = 12
+plt.rcParams['axes.labelsize'] = 10
+plt.rcParams['xtick.labelsize'] = 8
+plt.rcParams['ytick.labelsize'] = 8
+plt.rcParams['font.sans-serif'] = 'Arial'
+dpi_res = 600
+fig_width = 6.9 # in
+fig_height = 9 # in
+ext = 'png'
+# ============================= #
+# ============================= #
+# spaces to plot: MyMZ, FyFz,THPHI,MxFx
+
+def nl(x,k_scale=80):
+    k = 1/np.percentile(x,k_scale)
+    z = 2/(1+np.exp(-k*x)) - 1
+    invnl = lambda y: -1/k*(np.log(1-y)-np.log(1+y))
+    return z,invnl
+
+def plot_single_joint_space(var1,var2,r,cbool,bins,bin_stretch=True,ax=None):
+
+    idx = np.logical_and(np.isfinite(var1),np.isfinite(var2)).ravel()
+    if bin_stretch:
+        var1s = np.empty(var1.shape[0])
+        var2s = np.empty(var2.shape[0])
+        var1s[idx], logit_y = nl(var1[idx], 90)
+        var2s[idx], logit_z = nl(var2[idx], 90)
+    else:
+        var1s = var1
+        var2s = var2
+
+    response, var1_edges, var2_edges = varTuning.joint_response_hist(var1s, var2s, r, cbool, bins=bins, min_obs=10)
+    if bin_stretch:
+        var1_edges = logit_y(var1_edges)
+        var2_edges = logit_z(var2_edges)
+    else:
+        pass
+
+    h= plt.pcolormesh(var1_edges[:-1], var2_edges[:-1], response, cmap='OrRd')
+    if ax is None:
+        ax = plt.gca()
+
+    ax.axvline(color='k', linewidth=1)
+    ax.axhline(color='k', linewidth=1)
+    ax.patch.set_color([0.6, 0.6, 0.6])
+
+    mask = response.mask.__invert__()
+    if not mask.all():
+        ax.set_ylim(var2_edges[np.where(mask)[0].min()], var2_edges[np.where(mask)[0].max()])
+        ax.set_xlim(var1_edges[np.where(mask)[1].min()], var1_edges[np.where(mask)[1].max()])
+    return(h)
+
+
+def plot_joint_spaces(blk,unit_num=0,bin_stretch=False,p_save=None):
+    id = neoUtils.get_root(blk,unit_num)
+    use_flags = neoUtils.concatenate_epochs(blk)
+    cbool = neoUtils.get_Cbool(blk)
+    r,b = neoUtils.get_rate_b(blk,unit_num,2*pq.ms)
+
+    M = neoUtils.get_var(blk, 'M').magnitude
+    F = neoUtils.get_var(blk, 'F').magnitude
+    TH = neoUtils.get_var(blk, 'TH').magnitude
+    PH = neoUtils.get_var(blk, 'PHIE').magnitude
+
+    TH = neoUtils.center_var(TH,use_flags)
+    PH = neoUtils.center_var(PH, use_flags)
+
+    TH[np.invert(cbool)] = np.nan
+    PH[np.invert(cbool)] = np.nan
+
+    vars = {}
+    names = {}
+    vars[0] = [M[:, 1].ravel(), M[:, 2].ravel()]
+    vars[1] = [F[:, 1].ravel(), F[:, 2].ravel()]
+    vars[2] = [M[:, 0].ravel(), F[:, 0].ravel()]
+    vars[3] = [TH.ravel(), PH.ravel()]
+
+    names[0] = ['M_y', 'M_z']
+    names[1] = ['F_y', 'F_z']
+    names[2] = ['M_x', 'F_x']
+    names[3] = ['\\Delta\\theta', '\\Delta\\phi']
+    titles = ['Bending','Lateral','Axial','Rotation']
+    f = plt.figure()
+    handles=[]
+    for ii,var in vars.iteritems():
+        ax = f.add_subplot(2,2,ii+1)
+        handles.append(plot_single_joint_space(var[0],var[1],r,cbool,50,ax=ax,bin_stretch=False))
+        ax.set_xlabel('${}$'.format(names[ii][0]))
+        ax.set_ylabel('${}$'.format(names[ii][1]))
+
+        ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.0e'))
+        ax.xaxis.set_major_formatter(mtick.FormatStrFormatter('%.0e'))
+        plt.xticks(rotation=25)
+        ax.set_title(titles[ii])
+
+
+        if ii==3:
+            ax.axis('equal')
+            max_color = np.max([x.get_clim() for x in handles])
+
+    # set all colors to same scale
+    for h in handles:
+        h.set_clim(0,max_color)
+
+    plt.colorbar(handles[0])
+    plt.suptitle('{}'.format(id))
+    plt.tight_layout()
+    if p_save is not None:
+        plt.savefig(os.path.join(p_save,'{}_joint_spaces.png'.format(id)),dpi=dpi_res)
+        plt.close('all')
+    return(0)
+
+def main(p_load,p_save):
+    for f in glob.glob(os.path.join(p_load,'*.h5')):
+        print('Working on {}'.format(os.path.basename(f)))
+        blk = neoUtils.get_blk(f)
+        num_units = len(blk.channel_indexes[-1].units)
+        for unit_num in range(num_units):
+            try:
+                plot_joint_spaces(blk,unit_num,p_save=p_save)
+            except:
+                print('Issue with {}'.format(os.path.basename(f)))
+
+if __name__=='__main__':
+    p_load = os.path.join(os.environ['BOX_PATH'],r'__VG3D\_deflection_trials\_NEO')
+    p_save = os.path.join(os.environ['BOX_PATH'], r'__VG3D\_deflection_trials\_NEO\results')
+    main(p_load,p_save)
--- scripts/derivative_analysis.py
+++ scripts/derivative_analysis.py
@@ -1,26 +1,58 @@
 import neoUtils
 import numpy as np
 import varTuning
-import spikeAnalysis
 import quantities as pq
 import os
 import matplotlib.pyplot as plt
+import glob
+import sys
 
-# blk_smooth = neoUtils.get_blk(r"C:\Users\nbush\Documents\rat2017_08_FEB15_VG_D1_NEO_smooth_dat.h5")
-# blk = neoUtils.get_blk()
+def mult_join_plots(var1,var2,r,cbool,bins=50):
+    if type(bins)==int:
+        bins = [np.linspace(np.nanmin(var1), np.nanmax(var1), bins),
+                np.linspace(np.nanmin(var2), np.nanmax(var2), bins)]
 
-blk_smooth = neoUtils.get_blk(r"F:\VG3D\_rerun_with_pad\_deflection_trials\_NEO\smooth\rat2017_09_FEB17_VG_C4_NEO_smooth_dat.h5")
-blk = neoUtils.get_blk(os.path.join(r'D:\Users\NBUSH\Box Sync\Box Sync\__VG3D\_deflection_trials\_NEO','rat2017_09_FEB17_VG_C4_NEO.h5'))
-def mult_join_plots(var):
-    pass
+    R = {}
+    xx=[]
+    yy=[]
+
+
+    for ii in range(var1.shape[-1]):
+        R[ii] = varTuning.joint_response_hist(var1[:,ii],var2[:,ii],r,cbool,bins=bins)[0]
+    for mesh in R.values():
+        rows = np.where(np.any(~mesh.mask, axis=0))[0]
+        cols = np.where(np.any(~mesh.mask, axis=1))[0]
+
+        xx.append([rows[0],rows[-1]])
+        yy.append([cols[0], cols[-1]])
+
+
+    xx = [np.min(xx), np.max(xx)]
+    yy = [np.min(yy), np.max(yy)]
 
 
 
-def plot_smooth_hists(blk,blk_smooth,unit_num=0):
+    return(R,bins,xx,yy)
+
+def plot_smooth_hists(blk,blk_smooth,unit_num=0,p_save=None,nbins=75):
+    DPI_RES=600
+    id = neoUtils.get_root(blk, unit_num)
+    fig_name = os.path.join(p_save, '{}_derivative_smoothing_compare.png'.format(id))
+    if os.path.isfile(fig_name):
+        print('{} found, skipping...'.format(fig_name))
+        return(None)
+
+    smoothing_windows = range(5,101,10)
     use_flags = neoUtils.concatenate_epochs(blk)
     cbool = neoUtils.get_Cbool(blk)
     r,b =neoUtils.get_rate_b(blk,unit_num,2*pq.ms)
 
+    # catch empty smoothed data
+    if len(blk_smooth.segments)==0 or len(blk_smooth.segments[0].analogsignals)==0:
+        print('Smoothed data not found in {}'.format(id))
+        return(-1)
+
+    # get vars
     M = neoUtils.get_var(blk_smooth,'M_smoothed').magnitude
     M[np.invert(cbool),:]=np.nan
     Mdot = neoUtils.get_deriv(M)
@@ -42,33 +74,102 @@ def plot_smooth_hists(blk,blk_smooth,unit_num=0):
     # ROT = np.sqrt(np.add(np.power(PHI,2),np.power(TH,2)))
     # ROTdot = neoUtils.get_deriv(ROT)
 
-    colormax = np.nanmax(r)/2
-    f = plt.figure()
-    nbins=50
-    for loc,ii in enumerate(range(0,10,2)):
-        R,edges1,edges2 = varTuning.joint_response_hist(Mdot[:,1,ii],Mdot[:,2,ii],r,cbool,bins=nbins)
-        ax = f.add_subplot(3,M.shape[-1]/2,loc+1)
-        ax.pcolormesh(edges1[:-1],edges2[:-1], R, cmap='OrRd', edgecolors='None',vmin=0,vmax=colormax)
-        ax.set_xlim(np.nanmin(Mdot[:,1,:]),np.nanmax(Mdot[:,1,:]))
-        ax.set_ylim(np.nanmin(Mdot[:, 2,:]), np.nanmax(Mdot[:, 2, :]))
-        ax.set_title('Smooth_level = {}'.format(ii))
-        if ii==0:
-            ax.set_ylabel('Mydot vs Mzdot')
-    for loc,ii in enumerate(range(0,10,2)):
-        R, edges1, edges2 = varTuning.joint_response_hist(Fdot[:, 1, ii], Fdot[:, 2, ii], r, cbool,bins=nbins)
-        ax = f.add_subplot(3, M.shape[-1]/2, loc + 1+5)
-        ax.pcolormesh(edges1[:-1], edges2[:-1], R, cmap='OrRd', edgecolors='None',vmin=0,vmax=colormax)
-        ax.set_xlim(np.nanmin(Fdot[:, 1, :]), np.nanmax(Fdot[:, 1, :]))
-        ax.set_ylim(np.nanmin(Fdot[:, 2, :]), np.nanmax(Fdot[:, 2, :]))
-        if ii==0:
-            ax.set_ylabel('Fydot vs Fzdot')
-    for loc,ii in enumerate(range(0,10,2)):
-        R, edges1, edges2 = varTuning.joint_response_hist(THdot[:, ii], PHIdot[:, ii], r, cbool,bins=nbins)
-        ax = f.add_subplot(3, M.shape[-1]/2, loc + 1+10)
-        ax.pcolormesh(edges1[:-1], edges2[:-1], R, cmap='OrRd', edgecolors='None',vmin=0,vmax=colormax)
-        ax.set_xlim(np.nanmin(THdot), np.nanmax(THdot))
-        ax.set_ylim(np.nanmin(PHIdot), np.nanmax(PHIdot))
-        if ii==0:
-            ax.set_ylabel('THdot vs PHIdot')
 
-    plt.show()
+    # calculate histograms
+    R_Mdot, bins_Mdot, edgesx_Mdot, edgesy_Mdot = mult_join_plots(Mdot[:, 1, :], Mdot[:, 2, :], r, cbool, bins=nbins)
+    newbins =[np.linspace(bins_Mdot[0][edgesx_Mdot][0],bins_Mdot[0][edgesx_Mdot][1],nbins),
+              np.linspace(bins_Mdot[1][edgesy_Mdot][0], bins_Mdot[1][edgesy_Mdot][1], nbins)]
+    R_Mdot, bins_Mdot, edgesx_Mdot, edgesy_Mdot = mult_join_plots(Mdot[:, 1, :], Mdot[:, 2, :], r, cbool, bins=newbins)
+
+    R_Fdot, bins_Fdot, edgesx_Fdot, edgesy_Fdot = mult_join_plots(Fdot[:, 1, :], Fdot[:, 2, :], r, cbool,bins=nbins)
+    newbins = [np.linspace(bins_Fdot[0][edgesx_Fdot][0], bins_Fdot[0][edgesx_Fdot][1], nbins),
+               np.linspace(bins_Fdot[1][edgesy_Fdot][0], bins_Fdot[1][edgesy_Fdot][1], nbins)]
+    R_Fdot, bins_Fdot, edgesx_Fdot, edgesy_Fdot = mult_join_plots(Fdot[:, 1, :], Fdot[:, 2, :], r, cbool, bins=newbins)
+
+    R_ROTdot, bins_ROTdot, edgesx_ROTdot, edgesy_ROTdot = mult_join_plots(THdot, PHIdot, r, cbool,bins=nbins)
+    newbins = [np.linspace(bins_ROTdot[0][edgesx_ROTdot][0], bins_ROTdot[0][edgesx_ROTdot][1], nbins),
+               np.linspace(bins_ROTdot[1][edgesy_ROTdot][0], bins_ROTdot[1][edgesy_ROTdot][1], nbins)]
+    R_ROTdot, bins_ROTdot, edgesx_ROTdot, edgesy_ROTdot = mult_join_plots(THdot, PHIdot, r, cbool, bins=newbins)
+
+    FR = []
+    FR.append(np.nanmax([x.max() for x in R_Mdot.values()]))
+    FR.append(np.nanmax([x.max() for x in R_Fdot.values()]))
+    FR.append(np.nanmax([x.max() for x in R_ROTdot.values()]))
+    colormax = np.nanmax(FR)
+
+    # Plots
+    f = plt.figure()
+    figManager = plt.get_current_fig_manager()
+    figManager.window.showMaximized()
+    # hardcoded for 5 smoothing steps
+    for loc,ii in enumerate(range(0,10,2)):
+        ax = f.add_subplot(3,5,loc+1)
+        ax.pcolormesh(bins_Mdot[0],bins_Mdot[1],R_Mdot[ii], cmap='OrRd', edgecolors='None',vmin=0,vmax=colormax)
+        ax.set_xlim(bins_Mdot[0][edgesx_Mdot])
+        ax.set_ylim(bins_Mdot[1][edgesy_Mdot])
+        ax.set_title('Smoothing window = {}ms'.format(smoothing_windows[ii]))
+        ax.axvline(color='k',linewidth=1)
+        ax.axhline(color='k',linewidth=1)
+        if ii==0:
+            ax.set_ylabel('$\\dot{M_y}$ vs  $\\dot{M_z}$',rotation=0,labelpad=20)
+    for loc,ii in enumerate(range(0,10,2)):
+        ax = f.add_subplot(3, 5, loc + 1+5)
+        ax.pcolormesh(bins_Fdot[0], bins_Fdot[1], R_Fdot[ii], cmap='OrRd', edgecolors='None', vmin=0, vmax=colormax)
+        ax.set_xlim(bins_Fdot[0][edgesx_Fdot])
+        ax.set_ylim(bins_Fdot[1][edgesy_Fdot])
+        ax.axvline(color='k', linewidth=1)
+        ax.axhline(color='k', linewidth=1)
+        if ii==0:
+            ax.set_ylabel('$\\dot{F_y}$ vs $\\dot{F_z}$',rotation=0,labelpad=20)
+    for loc,ii in enumerate(range(0,10,2)):
+        ax = f.add_subplot(3, 5, loc + 1+10)
+        h=ax.pcolormesh(bins_ROTdot[0], bins_ROTdot[1], R_ROTdot[ii], cmap='OrRd', edgecolors='None', vmin=0, vmax=colormax)
+        ax.set_xlim(bins_ROTdot[0][edgesx_ROTdot])
+        ax.set_ylim(bins_ROTdot[1][edgesy_ROTdot])
+        ax.axvline(color='k', linewidth=1)
+        ax.axhline(color='k', linewidth=1)
+        if ii==0:
+            ax.set_ylabel('$\\dot{\\theta}$ vs $\\dot{\\phi}$',rotation=0,labelpad=20)
+    plt.suptitle('{}'.format(id))
+    plt.colorbar(h)
+    plt.pause(0.1)
+
+    if p_save is not None:
+        plt.savefig(fig_name,dpi=DPI_RES)
+        plt.close('all')
+    return(None)
+
+def find_smooth_match(fname,p_smooth):
+    f_smooth = [os.path.splitext(os.path.basename(x))[0] for x in glob.glob(os.path.join(p_smooth,'*smooth_dat.h5'))]
+    root = os.path.splitext(os.path.basename(fname))[0]
+    return([s for s in f_smooth if root in s])
+
+
+
+def main(p_raw,p_smooth,p_save):
+    print('p_raw: {}\np_smooth:{}\np_save:{}'.format(p_raw,p_smooth,p_save))
+    for f in glob.glob(os.path.join(p_raw,'*.h5')):
+        f_smooth = find_smooth_match(f,p_smooth)
+        if len(f_smooth)==0:
+            print('{} smooth correspondant not found'.format(f))
+            continue
+        else:
+            f_smooth =f_smooth[0]+'.h5'
+            print('Working on {}'.format(os.path.basename(f)))
+        blk = neoUtils.get_blk(os.path.join(p_raw, f))
+        blk_smooth = neoUtils.get_blk(os.path.join(p_smooth, f_smooth))
+        num_units = len(blk.channel_indexes[-1].units)
+        for unit_num in xrange(num_units):
+            try:
+                res= plot_smooth_hists(blk,blk_smooth,unit_num,p_save)
+            except:
+                print('Failed at {}'.format(f))
+
+
+if __name__ == '__main__':
+
+    p_raw = sys.argv[1]
+    p_smooth = sys.argv[2]
+    p_save = sys.argv[3]
+    main(p_raw,p_smooth,p_save)
+
