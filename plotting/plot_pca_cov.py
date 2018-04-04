a307faa20432720f98056543c5e6b5c79c819597
nbush257 <nbush257@gmail.com>
2018-04-04 11:22:25 -0500

    plotting of pca takes the information from the pillow data, also uses the new plotVG3D set fig style, and some style changes

--- plotting/plot_pca_cov.py
+++ plotting/plot_pca_cov.py
@@ -1,39 +1,49 @@
 from analyze_by_deflection import *
+import plotVG3D
+import os
+import seaborn as sns
 # ============================ #
+
 # edit here #
 # ============================ #
-save_loc = os.path.join(os.environ['BOX_PATH'],r'__VG3D/_deflection_trials/_NEO/results')
+save_loc = os.path.join(os.environ['BOX_PATH'],r'__VG3D/_deflection_trials/_NEO/pillowX')
 cell_list = ['201711B2','201708D1']
-plt.rcParams['svg.fonttype'] = 'none'
-plt.rcParams['axes.titlesize'] = 12
-plt.rcParams['figure.titlesize'] = 12
-plt.rcParams['axes.labelsize'] = 10
-plt.rcParams['xtick.labelsize'] = 8
-plt.rcParams['ytick.labelsize'] = 8
-plt.rcParams['font.sans-serif'] = 'Arial'
-dpi_res = 600
-fig_width = 6.9 # in
-fig_height = 9 # in
-ext = 'png'
+figsize = plotVG3D.set_fig_style()[1]
 sns.set_style('ticks')
 # =================================
 # Plot PCA variance explained
-dat = np.load(os.path.join(os.environ['BOX_PATH'],r'__VG3D\_deflection_trials\_NEO\results\cov_exp_var.npz'))
-wd = fig_width/3
+dat = np.load(os.path.join(save_loc,'input_pca.npy'))
+wd = figsize[0]/3
 ht = wd/.75
 f = plt.figure(figsize=(wd,ht))
-exp_var = dat['exp_var']
+exp_var = np.array([x.explained_variance_ratio_ for x in dat]).T
 plt.plot(exp_var,'k',alpha=.15,linewidth=1)
-plt.plot(np.mean(exp_var,axis=1),color='r',linewidth=3)
+plt.plot(np.mean(exp_var,axis=1),'o-',color = [182/255.,0,0],linewidth=2,markeredgecolor='k',markeredgewidth=1)
 sns.despine()
 plt.ylim(0,1)
 plt.yticks([0,.5,1])
 plt.xlabel('Principal Component')
 plt.ylabel('Variance Explained')
 plt.tight_layout()
-plt.savefig(os.path.join(save_loc,'PCA_var_explained.{}'.format(ext)),dpi=dpi_res)
+plt.savefig(os.path.join(save_loc,'PCA_var_explained.pdf'))
 plt.close('all')
-
+# ==================================
+# Plot cumulative variance explained
+wd = figsize[0]/2
+ht = wd/.75
+f = plt.figure(figsize=(wd,ht))
+cum_exp_var = np.cumsum(exp_var,axis=0)
+plt.plot(cum_exp_var,'k',alpha=.15,linewidth=1)
+plt.plot(np.mean(cum_exp_var,axis=1),'o-',color = [182/255.,0,0],linewidth=2,markeredgecolor='k',markeredgewidth=1)
+sns.despine()
+plt.ylim(0,1.1)
+plt.yticks([0,.5,1])
+plt.xlabel('Principal Component')
+plt.ylabel('Cumulative Variance Explained')
+plt.axhline(0.95,color='k',ls='--')
+plt.tight_layout()
+plt.savefig(os.path.join(save_loc,'cumulative_PCA_var_explained.pdf'))
+plt.close('all')
 # ================================
 # Plot Covariance matrices
 sns.set_style('white')
@@ -53,7 +63,10 @@ for cell in cell_list:
     plt.xticks(np.arange(0.5,8),dat['var_labels'].tolist())
     plt.yticks(np.arange(0.5,8),dat['var_labels'].tolist(),rotation=0)
     plt.title('Variable covariance {}'.format(cell))
-   plt.tight_layout()
+    plt.tight_layout()
     plt.savefig(os.path.join(save_loc,'var_covariance_{}.{}'.format(cell,ext)),dpi=dpi_res)
     plt.close('all')
+
+# ========================
+# Plot canonical angles
 
