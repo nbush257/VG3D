c7952bb9e1f548ccd8448fbebc1694bd760ff01d
nbush257 <nbush257@gmail.com>
2018-04-06 14:56:58 -0500

    edited the pillow result script to save csvs of the weights and orthogonalized weights

--- scripts/pillow_result.py
+++ scripts/pillow_result.py
@@ -60,9 +60,13 @@ def get_canonical_angles(fname):
 def batch_mat_to_dataframes():
     p_load = os.path.join(os.environ['BOX_PATH'],r'__VG3D\_deflection_trials\_NEO\pillowX')
     kernel_sizes = np.power(2,np.arange(1,10))
+    varnames = ['Mx','My','Mz','Fx','Fy','Fz','TH','PHI',
+                'Mxdot', 'Mydot', 'Mzdot', 'Fxdot', 'Fydot', 'Fzdot', 'THdot', 'PHIdot']
 
-    DF_weights = pd.DataFrame()
+    DF_canonical = pd.DataFrame()
     DF_performance = pd.DataFrame()
+    DF_pillow_weights = pd.DataFrame()
+    DF_pillow_weights_normed = pd.DataFrame()
     pca_all_whiskers = []
     for f in glob.glob(os.path.join(p_load,'*MID.mat')):
         id = os.path.basename(f)[:10]
@@ -75,16 +79,37 @@ def batch_mat_to_dataframes():
         df_r['kernel_sizes'] = kernel_sizes
         df_r['id'] = id
 
-        df_weights = pd.DataFrame()
-        df_weights['Angle0'] = [canonical_angles[0]]
-        df_weights['Angle1'] = [canonical_angles[1]]
-        df_weights['Angle2'] = [canonical_angles[2]]
-        df_weights['id'] = id
-        DF_weights = DF_weights.append(df_weights)
+        df_canonical = pd.DataFrame()
+        df_canonical['Angle0'] = [canonical_angles[0]]
+        df_canonical['Angle1'] = [canonical_angles[1]]
+        df_canonical['Angle2'] = [canonical_angles[2]]
+        df_canonical['id'] = id
+
+        df_pillow_weights = pd.DataFrame()
+        df_pillow_weights_normed = pd.DataFrame()
+        for ii in range(3):
+            df_pillow_weights['Filter_{}'.format(ii)] = weight_dict['K'][:,ii]
+        df_pillow_weights['var'] = varnames
+        df_pillow_weights['id'] = id
+
+        df_pillow_weights_normed = pd.DataFrame()
+        for ii in range(3):
+            df_pillow_weights_normed['Filter_{}'.format(ii)] = weight_dict['Ko'][:,ii]
+        df_pillow_weights_normed['var'] = varnames
+        df_pillow_weights_normed['id'] = id
+
+        DF_pillow_weights = DF_pillow_weights.append(df_pillow_weights)
+        DF_pillow_weights_normed = DF_pillow_weights_normed.append(df_pillow_weights_normed)
+        DF_canonical = DF_canonical.append(df_canonical)
         DF_performance = DF_performance.append(df_r)
         pca_all_whiskers.append(weight_dict['pc'])
+
+
     DF_performance.to_csv(os.path.join(p_load,'pillow_MID_performance.csv'))
-    DF_weights.to_csv(os.path.join(p_load,'pillow_MID_canonical_angles.csv'))
+    DF_canonical.to_csv(os.path.join(p_load,'pillow_MID_canonical_angles_weights_against_pca.csv'))
+    DF_pillow_weights.to_csv(os.path.join(p_load,'pillow_MID_weights_raw.csv'))
+    DF_pillow_weights_normed.to_csv(os.path.join(p_load,'pillow_MID_weights_orthogonalized.csv'))
+
     np.save(os.path.join(p_load,'input_pca.npy'),pca_all_whiskers)
 
 if __name__=='__main__':
